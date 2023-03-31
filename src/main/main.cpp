bca8f2a5e2e42845df3eabce45b65b0e34070fd2
Jie Meng <jie.meng@total.com>
2023-03-31 18:41:49 -0500

    with caliper enabled

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -8,34 +8,45 @@
 #include "utils.hpp"
 #include "solver.hpp"
 
+#include "commonMacro.hpp"
+
 using namespace std;
 
 int main()
-{
-    int ex;
+{ int ex;
     int ey;
     int order;
     float lx;
     float ly;
     float hx,hy;
     
+    SEM_CALIPER_MARK_BEGIN("generate mesh");
     QkGL Qk;
     simpleMesh mesh {ex=100,ey=100,lx=2000,ly=2000,order=2};
+    SEM_CALIPER_MARK_END("generate mesh");
+
     solver solve; 
     solverUtils utils;
 
-    float timeMax=1;
+    //float timeMax=1;
+    float timeMax=0.002;
     float timeStep=0.001;
     int nSamples=timeMax/timeStep;
     int indexTimeStepSource=nSamples;
     // iniatialize source term
     float f0=15.;
     int sourceOrder=1;
+
+    SEM_CALIPER_MARK_BEGIN("compute sourceTerm");
     vector<float>sourceTerm=utils.computeSourceTerm(nSamples, timeStep, f0,sourceOrder );
     for ( int i=0; i<nSamples;i++)
     {
         if( i%100==0)cout<<"sample "<<i<<" sourceTerm="<<sourceTerm[i]<<endl;
     }
+    SEM_CALIPER_MARK_END("compute sourceTerm");
+
+
+    SEM_CALIPER_MARK_BEGIN("set location");
     // set number of rhs and location
     int numberOfRHS=1;
     vector<vector<float>>rhsLocation(numberOfRHS,vector<float>(2));
@@ -43,6 +54,7 @@ int main()
     rhsLocation[0][0]=501;
     rhsLocation[0][1]=501;
     cout << "source location "<<rhsLocation[0][0]<<", "<<rhsLocation[0][1]<<endl;
+
     // get element number of source term
     float x=rhsLocation[0][0];
     float y=rhsLocation[0][1];
@@ -52,6 +64,7 @@ int main()
     {
         rhsTerm[0][j]=sourceTerm[j];
     }
+    SEM_CALIPER_MARK_END("set location");
 
 
     // loop over time
@@ -64,21 +77,33 @@ int main()
     int ny=mesh.getNy();
     vector<vector<int>> nodeList=mesh.globalNodesList(numberOfElements);
     vector<vector<float>> pnGlobal(numberOfNodes,vector<float> (2));
+
     for (int indexTimeStep=0; indexTimeStep<nSamples;indexTimeStep++)
     {       
+        SEM_CALIPER_MARK_BEGIN("solve.addRightAndSides");
         solve.addRightAndSides(indexTimeStep,numberOfRHS,i2,timeStep,pnGlobal,rhsTerm,rhsLocation,mesh);
+        SEM_CALIPER_MARK_END("solve.addRightAndSides");
+
+        SEM_CALIPER_MARK_BEGIN("solve.computeOneStep");
         solve.computeOneStep(timeStep,order,i1,i2,pnGlobal,mesh,Qk);
+        SEM_CALIPER_MARK_END("solve.computeOneStep");
+
+
         //writes debugging ascii file.
+        SEM_CALIPER_MARK_BEGIN("utils.saveSnapShot");
         if (indexTimeStep%40==0)
         {  
            cout<<indexTimeStep<<" i1="<<i1<<" i2="<<i2<<endl;
            cout<<"pnGlobal @ elementSource location "<<elementSource<<" after computeOneStep ="<<pnGlobal[nodeList[elementSource][0]][i2]<<endl;
            utils.saveSnapShot(indexTimeStep,i1,pnGlobal,mesh);
         }
+        SEM_CALIPER_MARK_END("utils.saveSnapShot");
+
         int tmp;
         tmp=i1;
         i1=i2;
         i2=tmp;
     }
+ 
 }
 
