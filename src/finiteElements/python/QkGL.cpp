092bf227a2bcd18c4b26ca50cd2242a124165596
henri calandra <henricalandra@netscape.net>
2023-06-10 08:30:21 +0200

    add python repos in finiteElements and mesh repos

---
+++ src/finiteElements/python/QkGL.cpp
@@ -0,0 +1,16 @@
+
+ #include "../../finiteElements/QkGL.hpp"
+ #include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(pyQkGL, m) 
+{
+    // optional module docstring
+    m.doc() = "QkGL python interface";
+
+    // bindings to simpleMesh class
+    py::class_<QkGL>(m, "QkGL")
+        .def(py::init< >());
+    
+}
