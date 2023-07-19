e93072694d5bb848d017afe251d52841b1f7adec
Jie Meng <jie.meng@total.com>
2023-07-19 02:45:03 -0500

    add script for compilation all the executables

--- scripts/make_executable.sh
+++ scripts/make_executable.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
- rm -rf ../build/* ; cmake -DCMAKE_INSTALL_PREFIX=../install .. -DSEM_USE_OMP=ON -DSEM_USE_LVARRAY=ON; make install
- rm -rf ../build/* ; cmake -DCMAKE_INSTALL_PREFIX=../install .. -DSEM_USE_OMP=ON -DSEM_USE_VECTOR=ON; make install
- rm -rf ../build/* ; cmake -DCMAKE_INSTALL_PREFIX=../install .. -DSEM_USE_RAJA=ON -DSEM_USE_LVARRAY=ON; make install
- rm -rf ../build/* ; cmake -DCMAKE_INSTALL_PREFIX=../install .. -DSEM_USE_RAJA=ON -DSEM_USE_VECTOR=ON; make install
- rm -rf ../build/* ; cmake -DCMAKE_INSTALL_PREFIX=../install .. -DSEM_USE_KOKKOS=ON -DSEM_USE_LVARRAY=ON; make install
- rm -rf ../build/* ; cmake -DCMAKE_INSTALL_PREFIX=../install .. -DSEM_USE_KOKKOS=ON -DSEM_USE_VECTOR=ON; make install
- rm -rf ../build/* ; cmake -DCMAKE_INSTALL_PREFIX=../install .. -DSEM_USE_LVARRAY=ON; make install
- rm -rf ../build/* ; cmake -DCMAKE_INSTALL_PREFIX=../install .. -DSEM_USE_VECTOR=ON; make install
+mkdir build; cd build
+
+ rm -rf ../build/* ; cmake -DCMAKE_INSTALL_PREFIX=../install .. -DSEM_USE_OMP=ON -DSEM_USE_LVARRAY=ON; make; make install
+ rm -rf ../build/* ; cmake -DCMAKE_INSTALL_PREFIX=../install .. -DSEM_USE_OMP=ON -DSEM_USE_VECTOR=ON; make; make install
+ rm -rf ../build/* ; cmake -DCMAKE_INSTALL_PREFIX=../install .. -DSEM_USE_RAJA=ON -DSEM_USE_LVARRAY=ON; make; make install
+ rm -rf ../build/* ; cmake -DCMAKE_INSTALL_PREFIX=../install .. -DSEM_USE_RAJA=ON -DSEM_USE_VECTOR=ON; make; make install
+ rm -rf ../build/* ; cmake -DCMAKE_INSTALL_PREFIX=../install .. -DSEM_USE_KOKKOS=ON -DSEM_USE_LVARRAY=ON; make; make install
+ rm -rf ../build/* ; cmake -DCMAKE_INSTALL_PREFIX=../install .. -DSEM_USE_KOKKOS=ON -DSEM_USE_VECTOR=ON; make; make install
+ rm -rf ../build/* ; cmake -DCMAKE_INSTALL_PREFIX=../install .. -DSEM_USE_LVARRAY=ON; make; make install
+ rm -rf ../build/* ; cmake -DCMAKE_INSTALL_PREFIX=../install .. -DSEM_USE_VECTOR=ON; make; make install
