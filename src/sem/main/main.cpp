1ac0706d3a80dd972fee2f34ffb4dad9edc7fd62
henri Calandra <henri.calandra@total.com>
2023-08-09 09:08:01 +0200

    finalize CMakeLists.txt

--- src/sem/main/main.cpp
+++ src/sem/main/main.cpp
@@ -7,10 +7,6 @@
 #include "semproxy.hpp"
 #include <chrono>
 
-#include "Array.hpp"
-#include "MallocBuffer.hpp"
-#include "ChaiBuffer.hpp"
-
 int main( int argc, char *argv[] )
 {
   SEM_CALIPER_MARK_BEGIN( "TotalTime" );
@@ -32,41 +28,5 @@ int main( int argc, char *argv[] )
   cout << "\n+================================= "<<endl;
   cout << "| SEM Application Finished.       "<<endl;
   cout << "+================================= \n"<<endl;
-
-  cout << "Elapsed Time : "<<chrono::duration_cast< chrono::milliseconds >( chrono::system_clock::now() - startTime ).count() / 1000.0 <<" seconds.\n"<<endl;
-  SEM_CALIPER_MARK_END( "TotalTime" );
-  LvArray::Array< int,
-                  2,
-                  camp::idx_seq< 1, 0 >,
-                  std::ptrdiff_t,
-                  LvArray::ChaiBuffer > array( 5, 6 );
-
-  // Move the array to the device.
-  array.move( LvArray::MemorySpace::cuda );
-  int * const devicePointer = array.data();
-
-  RAJA::forall< RAJA::cuda_exec< 32 > >(
-    RAJA::TypedRangeSegment< std::ptrdiff_t >( 0, array.size() ),
-    [devicePointer] __device__ ( std::ptrdiff_t const i )
-  {
-    devicePointer[ i ] = i;
-  }
-    );
-
-  LvArray::ArrayView< int,
-                      2,
-                      0,
-                      std::ptrdiff_t,
-                      LvArray::ChaiBuffer > const & view = array;
-
-  // Capture the view in a host kernel which moves the data back to the host.
-  RAJA::forall< RAJA::loop_exec >(
-    RAJA::TypedRangeSegment< std::ptrdiff_t >( 0, view.size() ),
-    [view] ( std::ptrdiff_t const i )
-  {
-    //EXPECT_EQ( view.data()[ i ], i );
-  }
-    );
-
 
 }
