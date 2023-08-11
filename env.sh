9af3b9fbe220ab9211c6e8f208a1fb9ea243c247
henri Calandra <henri.calandra@total.com>
2023-08-11 10:18:30 +0200

    add kokkos, still some improvment are necessary

--- env.sh
+++ env.sh
@@ -1,11 +1,9 @@
 export MODULEPATH=/data_local/appli_local/MTS/GEOSX/modulefiles/:$MODULEPATH
-#module load tpl/gcc8.4.1-ompi4.1.2-stable.1-release
 module load cmake/3.21.4 gcc/8.4.1 cuda/11.0.3 ompi/4.1.2 openblas/0.3.18 python4geosx/3.8.5-gcc-8.4.1-ompi-4.1.2
 
-#export SEM_TPL_ROOT_DIR=${GEOSX_TPL_DIR}
-export SEM_TPL_ROOT_DIR=/users/j0024947/appli_src/geosx/codes/proxyApp/tpl4ProxyApp/installTPLNOCUDA
-#export SEM_TPL_ROOT_DIR=/users/j0024947/appli_src/geosx/codes/proxyApp/tpl4ProxyApp/installTPL
+export SEM_TPL_ROOT_DIR=/users/j0024947/appli_src/geosx/codes/proxyApp/tpl4ProxyApp/installTPLCudaSupport
 export RAJA_DIR=${SEM_TPL_ROOT_DIR}/raja/lib/cmake/raja
 export CAMP_DIR=${SEM_TPL_ROOT_DIR}/raja/lib/cmake/camp
 export CHAI_DIR=${SEM_TPL_ROOT_DIR}/chai/share/chai/cmake/
 export UMPIRE_DIR=${SEM_TPL_ROOT_DIR}/chai/lib/cmake/umpire/
+export KOKKOS_DIR=${SEM_TPL_ROOT_DIR}/kokkos/lib64/cmake/Kokkos/
--- src/sem/utils/dataType.hpp
+++ src/sem/utils/dataType.hpp
@@ -39,98 +39,97 @@
   #include "Array.hpp"
   #ifdef SEM_USE_CUDA
     #include "ChaiBuffer.hpp"
-    using buffer=LvArray::ChaiBuffer;
-  #elif
-    #include "MallocBuffer.hpp"
-    using buffer=LvArray::MallocBuffer;
-  #endif
-
-
-  // Create an 1D array of integers.
-  using vectorInt=LvArray::Array< int,
-                                  1,
-                                  camp::idx_seq< 0 >,
-                                  std::ptrdiff_t,
-                                  buffer >;
-  using vectorReal=LvArray::Array< float,
-                                   1,
-                                   camp::idx_seq< 0 >,
+    // Create an 1D array of integers.
+    using vectorInt=LvArray::Array< int,
+                                    1,
+                                    camp::idx_seq< 0 >,
+                                    std::ptrdiff_t,
+                                    LvArray::ChaiBuffer >;
+    using vectorReal=LvArray::Array< float,
+                                     1,
+                                     camp::idx_seq< 0 >,
+                                     std::ptrdiff_t,
+                                     LvArray::ChaiBuffer >;
+    using vectorDouble=LvArray::Array< double,
+                                       1,
+                                       camp::idx_seq< 0 >,
+                                       std::ptrdiff_t,
+                                       LvArray::ChaiBuffer >;
+    using arrayInt=LvArray::Array< int,
+                                   2,
+                                   camp::idx_seq< 0, 1 >,
                                    std::ptrdiff_t,
-                                   LvArray::MallocBuffer >;
-  using vectorDouble=LvArray::Array< double,
+                                   LvArray::ChaiBuffer >;
+    using arrayReal=LvArray::Array< float,
+                                    2,
+                                    camp::idx_seq< 0, 1 >,
+                                    std::ptrdiff_t,
+                                    LvArray::ChaiBuffer >;
+    using arrayDouble=LvArray::Array< double,
+                                      2,
+                                      camp::idx_seq< 0, 1 >,
+                                      std::ptrdiff_t,
+                                      LvArray::ChaiBuffer >;
+  #else
+    #include "MallocBuffer.hpp"
+    // Create an 1D array of integers.
+    using vectorInt=LvArray::Array< int,
+                                    1,
+                                    camp::idx_seq< 0 >,
+                                    std::ptrdiff_t,
+                                    LvArray::MallocBuffer >;
+    using vectorReal=LvArray::Array< float,
                                      1,
                                      camp::idx_seq< 0 >,
                                      std::ptrdiff_t,
                                      LvArray::MallocBuffer >;
-  using arrayInt=LvArray::Array< int,
-                                 2,
-                                 camp::idx_seq< 0, 1 >,
-                                 std::ptrdiff_t,
-                                 LvArray::MallocBuffer >;
-  using arrayReal=LvArray::Array< float,
-                                  2,
-                                  camp::idx_seq< 0, 1 >,
-                                  std::ptrdiff_t,
-                                  LvArray::MallocBuffer >;
-  using arrayDouble=LvArray::Array< double,
+    using vectorDouble=LvArray::Array< double,
+                                       1,
+                                       camp::idx_seq< 0 >,
+                                       std::ptrdiff_t,
+                                       LvArray::MallocBuffer >;
+    using arrayInt=LvArray::Array< int,
+                                   2,
+                                   camp::idx_seq< 0, 1 >,
+                                   std::ptrdiff_t,
+                                   LvArray::MallocBuffer >;
+    using arrayReal=LvArray::Array< float,
                                     2,
                                     camp::idx_seq< 0, 1 >,
                                     std::ptrdiff_t,
                                     LvArray::MallocBuffer >;
-
-
+    using arrayDouble=LvArray::Array< double,
+                                      2,
+                                      camp::idx_seq< 0, 1 >,
+                                      std::ptrdiff_t,
+                                      LvArray::MallocBuffer >;
+  #endif
 #endif //SEM_USE_LVARRAY
 
 #ifdef SEM_USE_KOKKOS
   #include <Kokkos_Core.hpp>
   #ifdef SEM_USE_KOKKOSVECTOR
-  #include <Kokkos_Vector.hpp>
-  #include <Kokkos_OpenMP.hpp>
-
-  /*
-  template< class T > class Array2D
-  {
-  public:
-    Array2D( int numRows, int numCols ): data( numRows, std::vector< T >( numCols )) {}
-    Array2D(): data( 0, std::vector< T >( 0 )) {}
-
-    std::vector< T > & operator[]( int index )
+    #include <Kokkos_Vector.hpp>
+    template< class T > class Array2D
     {
+      public:
+        Array2D( int numRows, int numCols ): data( numRows, Kokkos::vector< T >( numCols )) {}
+        Array2D(): data( 0, Kokkos::vector< T >( 0 )) {}
+
+        Kokkos::vector< T > & operator[]( int index )
+      {
       return data[index];
-    }
+      }
 
-  private:
-    std::vector< std::vector< T > > data;
-  };
-  //*
-  using vectorInt=std::vector< int >;
-  using vectorReal=std::vector< float >;
-  using vectorDouble=std::vector< double >;
-  //*/
-  ///*
-  template< class T > class Array2D
-  {
-    public:
-      Array2D( int numRows, int numCols ): data( numRows, Kokkos::vector< T >( numCols )) {}
-      Array2D(): data( 0, Kokkos::vector< T >( 0 )) {}
-
-      Kokkos::vector< T > & operator[]( int index )
-    {
-      return data[index];
-    }
-
-    private:
+      private:
       Kokkos::vector< Kokkos::vector< T > > data;
-  };
-  //*/
-  ///*
-  using vectorInt=Kokkos::vector< int >;
-  using vectorReal=Kokkos::vector< float >;
-  using vectorDouble=Kokkos::vector< double >;
-  //*/
-  using arrayInt=Array2D< int >;
-  using arrayReal=Array2D< float >;
-  using arrayDouble=Array2D< double >;
+    };
+    using vectorInt=Kokkos::vector< int >;
+    using vectorReal=Kokkos::vector< float >;
+    using vectorDouble=Kokkos::vector< double >;
+    using arrayInt=Array2D< int >;
+    using arrayReal=Array2D< float >;
+    using arrayDouble=Array2D< double >;
   #endif //SEM_USE_KOKKOSVECTOR
 #endif //SEM_USE_KOKKOS
 
