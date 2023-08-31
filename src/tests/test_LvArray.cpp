f6684ec7573cbd1c2b7863c1aeaa592f218d7b9a
henri calandra <henricalandra@netscape.net>
2023-08-31 13:26:04 +0200

    in test_LvArray.cpp shows difference in argument function  between vector and LvArray

--- src/tests/test_LvArray.cpp
+++ src/tests/test_LvArray.cpp
@@ -14,12 +14,26 @@
 #include "MallocBuffer.hpp"
 #include "ChaiBuffer.hpp"
 
+template< class T > class Array2D
+{
+public:
+    Array2D( int numRows, int numCols ): data( numRows, std::vector< T >( numCols )) {}
+    Array2D(): data( 0, std::vector< T >( 0 )) {}
+
+    std::vector< T > & operator[]( int index ){return data[index];}
+    T& operator()(size_t row, size_t col) {return data[row][col];}
+
+  private:
+    std::vector< std::vector< T > > data;
+};
+
+using arrayReal=Array2D< float >;
 // test lvArray
 using arrayRealMB=LvArray::Array< float,
-                                 2,
-                                 camp::idx_seq< 1,0 >,
-                                 std::ptrdiff_t,
-                                 LvArray::MallocBuffer >;
+                        2,
+                       camp::idx_seq< 1,0 >,
+                       std::ptrdiff_t,
+                       LvArray::MallocBuffer > ;
 
 using arrayRealCB=LvArray::Array< float,
                                  2,
@@ -41,10 +55,25 @@ T allocateArray2D(int n1, int n2)
     T array(n1, n2);
     return array;
 }
+
+// test vector vector 
+//
+// -------------------------------
+void compute(int const n1, int const n2,arrayReal  & arrayIn, arrayReal   & array) 
+{
+  for (int i=0; i<n1;i++)
+    {
+      for ( int j=0; j<n2;j++)
+      {
+        array(i,j)=sqrt(2*i*j)*arrayIn[i][j];
+      }     
+    }
+}
+
 // test MallocBuffer MallocBuffer 
 //
 // -------------------------------
-void computeMBMB(int const n1, int const n2,arrayRealMB & arrayIn, arrayRealMB const & array) 
+void computeMBMB(int const n1, int const n2,arrayRealMB const & arrayIn, arrayRealMB const & array) 
 {
   for (int i=0; i<n1;i++)
     {
@@ -57,7 +86,7 @@ void computeMBMB(int const n1, int const n2,arrayRealMB & arrayIn, arrayRealMB c
 
 // test ChaiBuffer ChaiBuffer 
 // -------------------------------
-void computeCBCB(int const n1, int const n2, arrayRealCB & arrayIn, arrayRealCB const & array) 
+void computeCBCB(int const n1, int const n2, arrayRealCB const & arrayIn, arrayRealCB const & array) 
 {
   for (int i=0; i<n1;i++)
     {
@@ -75,10 +104,14 @@ int main( int argc, char *argv[] )
 {
   
   
-  const int nIter=1000;
+  const int nIter=10000;
   const int n1=1000;
   const int n2=1000;
 
+  arrayReal arrayIn;
+  arrayIn=allocateArray2D<arrayReal>(n1,n2);
+  arrayReal arrayOut;
+  arrayOut=allocateArray2D<arrayReal>(n1,n2);
   arrayRealCB arrayCBIn;
   arrayCBIn=allocateArray2D<arrayRealCB>(n1,n2);
   arrayRealCB arrayCBOut;
@@ -92,13 +125,28 @@ int main( int argc, char *argv[] )
   {
     for ( int j=0; j<n2;j++)
     {
+      arrayIn[i][j]=sqrt(2*i*j);
       arrayCBIn[i][j]=sqrt(2*i*j);
       arrayMBIn[i][j]=sqrt(2*i*j);
+      arrayOut[i][j]=0;
       arrayCBOut[i][j]=0;
       arrayMBOut[i][j]=0;
     }
   }
 
+  // test vector vector OMP
+  // -------------------------------
+  std::chrono::time_point< std::chrono::system_clock > startTime = std::chrono::system_clock::now();
+  #pragma omp parallel for
+  for (int e=0; e<nIter; e++)
+  {
+      compute(n1,n2,arrayIn, arrayOut) ; 
+      if (e==nIter-1)
+        std::cout <<"Result OMP vector "<<arrayOut[n1/2][n2/2]<<std::endl;   
+  }
+  std::cout << "Elapsed Time OMP loop vector : "<<std::chrono::duration_cast< std::chrono::milliseconds >
+            ( std::chrono::system_clock::now() - startTime ).count() / 1000.0 <<" seconds.\n"<<std::endl;
+  
 
   // test MallocBuffer MallocBuffer OMP
   // -------------------------------
@@ -130,7 +178,7 @@ int main( int argc, char *argv[] )
   // test MallocBuffer MallocBuffer RAJA
   // -------------------------------
   std::chrono::time_point< std::chrono::system_clock > startTimeRAJAMBMB = std::chrono::system_clock::now();
-  RAJA::forall< RAJA::omp_parallel_for_exec >( RAJA::RangeSegment( 0, nIter ), [=,&arrayMBIn] ( int e )
+  RAJA::forall< RAJA::omp_parallel_for_exec >( RAJA::RangeSegment( 0, nIter ), [=] ( int e )
   {
       computeMBMB(n1,n2,arrayMBIn,arrayMBOut) ;
       if (e==nIter-1)
@@ -143,7 +191,7 @@ int main( int argc, char *argv[] )
   // test ChaiBuffer ChaiBuffer RAJA
   // -------------------------------
   std::chrono::time_point< std::chrono::system_clock > startTimeRAJACBCB = std::chrono::system_clock::now();
-  RAJA::forall< RAJA::omp_parallel_for_exec >( RAJA::RangeSegment( 0, nIter ), [=,&arrayCBIn] ( int e )
+  RAJA::forall< RAJA::omp_parallel_for_exec >( RAJA::RangeSegment( 0, nIter ), [=] ( int e )
   {
       computeCBCB(n1,n2,arrayCBIn,arrayCBOut) ;
       if (e==nIter-1)
