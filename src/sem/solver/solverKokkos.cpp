b5692649cf72d538fc7ba4ef7821443f0a8220b0
Henri Calandra <henricalandra@netscape.net>
2023-10-25 16:47:36 +0200

    remove extra lines in solverKokkos.cpp

--- src/sem/solver/solverKokkos.cpp
+++ src/sem/solver/solverKokkos.cpp
@@ -28,83 +28,61 @@ void solverKokkos::computeOneStep( const int & timeStep,
     massMatrixGlobal[i]=0;
     yGlobal[i]=0;
   } );
-  //Kokkos::fence();
-
   // update pnGLobal with right hade side
   Kokkos::parallel_for(numberOfRHS,KOKKOS_CLASS_LAMBDA (const int i)
   {
     int nodeRHS=globalNodesList(rhsElement[i],0);
     pnGlobal(nodeRHS,i2)+=timeSample*timeSample*model[rhsElement[i]]*model[rhsElement[i]]*rhsTerm(i,timeStep);
   });
-  //Kokkos::fence();
- 
   Kokkos::parallel_for( numberOfElements, KOKKOS_CLASS_LAMBDA ( const int e )
   {
         // start parallel section
         int localToGlobal[36];
         double Xi[36][2];
-
         double jacobianMatrix[36][4];
         double detJ[36];
         double invJacobianMatrix[36][4];
         double transpInvJacobianMatrix[36][4];
-
         double B[36][4];
         double R[36][36];
-
         double massMatrixLocal[36];
-
         double pnLocal[36];
         double Y[36];
-
         // extract global coordinates of element e
         // get local to global indexes of nodes of element e
         int i=mesh.localToGlobalNodes( e, numberOfPointsPerElement, globalNodesList, localToGlobal );
-
         //get global coordinates Xi of element e
         int j=mesh.getXi( numberOfPointsPerElement, globalNodesCoords, localToGlobal, Xi );
-        //if(e==0)printf("threadId %d Xi %f %f %f %f %f %f %f %f\n",threadId, Xi(threadId,0,0),Xi(threadId,0,1),
-        //                                                                  Xi(threadId,1,0),Xi(threadId,1,1),
-        //                                                                  Xi(threadId,2,0),Xi(threadId,2,1),
-        //                                                                  Xi(threadId,3,0),Xi(threadId,3,1));
         // compute jacobian Matrix
         int k=Qk.computeJacobianMatrix( numberOfPointsPerElement, Xi,
                                         derivativeBasisFunction2DX,
                                         derivativeBasisFunction2DY,
                                         jacobianMatrix );
-
         // compute determinant of jacobian Matrix
         int l=Qk.computeDeterminantOfJacobianMatrix( numberOfPointsPerElement,
                                                      jacobianMatrix,
-                                                     detJ );
         // compute inverse of Jacobian Matrix
         int m=Qk.computeInvJacobianMatrix( numberOfPointsPerElement,
                                            jacobianMatrix,
                                            detJ,
                                            invJacobianMatrix );
-                                 
         // compute transposed inverse of Jacobian Matrix
         int n=Qk.computeTranspInvJacobianMatrix( numberOfPointsPerElement,
                                                  jacobianMatrix,
                                                  detJ,
                                                  transpInvJacobianMatrix );
-                        
         // compute  geometrical transformation matrix
         int p=Qk.computeB( numberOfPointsPerElement, invJacobianMatrix, transpInvJacobianMatrix, detJ,B );
-
         // compute stifness and mass matrix ( durufle's optimization)
         int q=Qk.gradPhiGradPhi( numberOfPointsPerElement, order, weights2D, B, derivativeBasisFunction1D, R );
-
         // compute local mass matrix ( used optimez version)
         int r=Qk.phiIphiJ( numberOfPointsPerElement, weights2D, detJ, massMatrixLocal );
-
         // get pnGlobal to pnLocal
         for( int i=0; i<numberOfPointsPerElement; i++ )
         {
           massMatrixLocal[i]/=(model[e]*model[e]);
           pnLocal[i]=pnGlobal(localToGlobal[i],i2);
         }
-
         // compute Y=R*pnLocal
         for( int i=0; i<numberOfPointsPerElement; i++ )
         {
@@ -114,19 +92,14 @@ void solverKokkos::computeOneStep( const int & timeStep,
             Y[i]+=R[i][j]*pnLocal[j];
           }
         }
-
         //compute gloval mass Matrix and global stiffness vector
         for( int i=0; i<numberOfPointsPerElement; i++ )
         {
           int gIndex=localToGlobal[i];
-          //massMatrixGlobal[gIndex]+=massMatrixLocal(threadId,i)
-          //yGlobal[gIndex]+=Y(threadId,i);
           Kokkos::atomic_add(&massMatrixGlobal[gIndex],massMatrixLocal[i]);
           Kokkos::atomic_add(&yGlobal[gIndex],Y[i]);
         } 
   });
-  //Kokkos::fence();
-
   // update pressure
   Kokkos::parallel_for( range_policy(0,numberOfInteriorNodes), KOKKOS_CLASS_LAMBDA ( const int i )
   {
@@ -135,13 +108,11 @@ void solverKokkos::computeOneStep( const int & timeStep,
     pnGlobal(I,i1)=2*pnGlobal(I,i2)-pnGlobal(I,i1)-tmp*yGlobal[I]/massMatrixGlobal[I];
   } );
   //Kokkos::fence();
-
   // damping terms
   Kokkos::parallel_for( range_policy(0,numberOfBoundaryNodes), KOKKOS_CLASS_LAMBDA ( const int i )
   {
     ShGlobal[i]=0;
   });
-
   Kokkos::parallel_for (numberOfBoundaryFaces, KOKKOS_CLASS_LAMBDA (const int iFace)
   {
       float ds[6];
@@ -163,8 +134,6 @@ void solverKokkos::computeOneStep( const int & timeStep,
         Kokkos::atomic_add(&ShGlobal[gIndexFaceNode],Sh[i]);
       }
   });
-  //Kokkos::fence();
-
   // update pressure @ boundaries;
   float tmp=timeSample*timeSample;
   Kokkos::parallel_for( range_policy(0,numberOfBoundaryNodes), KOKKOS_CLASS_LAMBDA  ( const int i )
