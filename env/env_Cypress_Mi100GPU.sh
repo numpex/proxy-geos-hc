a5ec8e6a203df4349485c80bab22310612587423
Jie Meng <jie.meng@total.com>
2024-02-11 16:10:34 -0600

    add kokkos+HIP on AMD GPUs

--- env/env_Cypress_Mi100GPU.sh
+++ env/env_Cypress_Mi100GPU.sh
@@ -6,8 +6,12 @@ export RAJA_DIR=${SEM_TPL_ROOT_DIR}/raja/lib/cmake/raja
 export CHAI_DIR=${SEM_TPL_ROOT_DIR}/chai/share/chai/cmake/
 export CAMP_DIR=${SEM_TPL_ROOT_DIR}/raja/lib/cmake/camp
 export UMPIRE_DIR=${SEM_TPL_ROOT_DIR}/chai/lib/cmake/umpire/
-export KOKKOS_DIR=${SEM_TPL_ROOT_DIR}/kokkos/lib64/cmake/Kokkos
-export KOKKOS_INCLUDE_DIR=${SEM_TPL_ROOT_DIR}/kokkos/include
+#export KOKKOS_DIR=${SEM_TPL_ROOT_DIR}/kokkos/lib64/cmake/Kokkos
+#export KOKKOS_INCLUDE_DIR=${SEM_TPL_ROOT_DIR}/kokkos/include
+
+export KOKKOS_DIR=/shared/data1/Users/j0535952/work2024/ProgrammingModels/kokkos/install_mi100_hipcc-6.0.0-gfx908/lib64/cmake/Kokkos
+export KOKKOS_INCLUDE_DIR=/shared/data1/Users/j0535952/work2024/ProgrammingModels/kokkos/install_mi100_hipcc-6.0.0-gfx908/include
+
 
 # don't forget to change CMakeLists.txt 
 # option (X86_cypress "Compilation on Cypress Cluster" ON)
