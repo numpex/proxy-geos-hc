5822670cd7e5e6d43e37d0dda2721c5f1c15d22c
Henri Calandra <henricalandra@netscape.net>
2024-04-26 15:42:16 +0200

    update env.sh

--- env/env.sh
+++ env/env.sh
@@ -1,10 +1,10 @@
 
-export _TPL_ROOT_DIR=~/src/proxyApp/buildInstall/installTPLCudaSupport
+export _TPL_ROOT_DIR=~/src/proxyApp/buildInstall/installTPLCuda
 export RAJA_DIR=${_TPL_ROOT_DIR}/raja/lib/cmake/raja
 export CAMP_DIR=${_TPL_ROOT_DIR}/raja/lib/cmake/camp
 export CHAI_DIR=${_TPL_ROOT_DIR}/chai/share/chai/cmake/
 export UMPIRE_DIR=${_TPL_ROOT_DIR}/chai/lib/cmake/umpire/
-kokkos_install_dir=~/src/proxyApp/buildInstall/installTPLCudaSupport/kokkos
+kokkos_install_dir=~/src/proxyApp/buildInstall/installTPLCuda/kokkos
 export KOKKOS_DIR=$kokkos_install_dir/lib/cmake/Kokkos
 export KOKKOS_INCLUDE_DIR=$kokkos_install_dir/include
 
