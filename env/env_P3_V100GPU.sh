9351cfaaacf1d45bad6815b80353de644e5c3ae8
jiemeng-total <jie.meng@totalenergies.com>
2024-04-30 22:31:08 +0200

    udpate environment for P3

--- env/env_P3_V100GPU.sh
+++ env/env_P3_V100GPU.sh
@@ -1,12 +1,13 @@
-#export MODULEPATH=/data_local/appli_local/MTS/GEOSX/modulefiles/:$MODULEPATH
-#module load cmake/3.21.4 gcc/8.4.1 cuda/11.0.3 ompi/4.1.2 openblas/0.3.18 python4geosx/3.8.5-gcc-8.4.1-ompi-4.1.2
+export MODULEPATH=/data_local/appli_local/MTS/GEOSX/modulefiles/:$MODULEPATH
+module load cmake/3.26.4 gcc cuda
 
-export SEM_TPL_ROOT_DIR=~/src/geosx/codes/proxyApp/buildInstall/installTPLCudaSupport
+export SEM_TPL_ROOT_DIR=/appli_RD/JIEMENG/SEMproxy/proxy_20240430/tpl4ProxyApp/installTPL_P3_GPU/
 export RAJA_DIR=${SEM_TPL_ROOT_DIR}/raja/lib/cmake/raja
-export CAMP_DIR=${SEM_TPL_ROOT_DIR}/raja/lib/cmake/camp
 export CHAI_DIR=${SEM_TPL_ROOT_DIR}/chai/share/chai/cmake/
+export CAMP_DIR=${SEM_TPL_ROOT_DIR}/raja/lib/cmake/camp
 export UMPIRE_DIR=${SEM_TPL_ROOT_DIR}/chai/lib/cmake/umpire/
-kokkos_install_dir=~/src/geosx/codes/proxyApp/buildInstall/installTPLCudaSupport/kokkos
-export KOKKOS_DIR=$kokkos_install_dir/lib64/cmake/Kokkos
-export KOKKOS_INCLUDE_DIR=$kokkos_install_dir/include
+export KOKKOS_DIR=${SEM_TPL_ROOT_DIR}/kokkos/lib64/cmake/Kokkos
+export KOKKOS_INCLUDE_DIR=${SEM_TPL_ROOT_DIR}/kokkos/include
+export CUDA_ROOT=/data_local/sw/cuda/11.0.3-rhel8/
+#
 
