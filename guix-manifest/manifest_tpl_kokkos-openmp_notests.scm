2e797d241891467ab7f74ea59773160cdaa1ee7c
Félix KPADONOU <felix.kpadonou@cea.fr>
2025-02-27 19:23:26 +0100

    Disabling tests in the Guix manifest, for Kokkos with OpenMP

---
+++ guix-manifest/manifest_tpl_kokkos-openmp_notests.scm
@@ -0,0 +1,35 @@
+;; Ce qui suit est un "manifeste" équivalent à la ligne de commande que vous avez donnée.
+;; Vous pouvez le stocker dans un fichier que vous pourrez ensuite passer à n'importe quelle
+;; commande 'guix' qui accepte une option '--manifest' (ou '-m').
+
+(use-modules (guix transformations))
+
+(define transform1
+  (options->transformation
+    '((without-tests . "kokkos-openmp"))))
+
+(packages->manifest
+  (list (transform1
+          (specification->package "kokkos-openmp"))
+        (specification->package "clang-toolchain@14")
+        (specification->package "python-sphinx")
+        (specification->package "graphviz")
+        (specification->package "doxygen")
+        (specification->package "uncrustify")
+        (specification->package "git")
+        (specification->package "grep")
+        (specification->package "sed")
+        (specification->package "valgrind")
+        (specification->package "python-yapf")
+        (specification->package "cmakelang")
+        (specification->package "cppcheck")
+        (specification->package "make")
+        (specification->package "astyle")
+        (specification->package "lesspipe")
+        (specification->package "cuda-toolkit")
+        (specification->package "vim")
+        (specification->package "binutils")
+        (specification->package "coreutils")
+        (specification->package "plocate")
+        (specification->package "cmake@3.25")
+        (specification->package "gcc-toolchain@11")))
