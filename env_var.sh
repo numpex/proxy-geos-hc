3afc70268130e06dba9d821b55f1f95658940823
Félix KPADONOU <felix.kpadonou@cea.fr>
2025-03-19 11:37:10 +0100

    remove build_tpl variable

--- env_var.sh
+++ env_var.sh
@@ -1,9 +1,7 @@
 # proxy_config_root - the root of the 'configs' folder which includes various platform-dependent .cmake files
-export proxy_config_root=`pwd` #'/path/to/proxy-geos-hc' if BUILD_FROM_TPLMIRROR is OFF otherwise /path/to/proxy-geos-hc_tpl
+export proxy_config_root='/path/to/proxy-geos-hc'
 # Name of the config file used to pre-load the cache. The absolute path is ${proxy_config_root}/configs/${config_proxy}
-export config_proxy='config_x86_guix.cmake'
-#export config_proxy='config_x86_rtx2000.cmake'
+export config_proxy='config_x86_rtx2000.cmake'
 
-# For BUILD_FROM_TPLMIRROR=ON, the build and install folders for the TPLs. It is supposed to be a subfolder of $ENV{proxy_config_root}. 
-export build_tpl='buildTPLs_O1_C1'
-export install_tpl='installTPLs_O1_C1'
+# For BUILD_FROM_TPLMIRROR=ON, the install folder for the TPLs. It is supposed to be a subfolder of $ENV{proxy_config_root}. 
+export install_tpl='installTPLs'
