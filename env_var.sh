582e3762483a0201ca2a645c80770277eb43182a
Félix KPADONOU <felix.kpadonou@cea.fr>
2025-03-19 13:19:09 +0100

    Edit

--- env_var.sh
+++ env_var.sh
@@ -1,7 +1,10 @@
-# proxy_config_root - the root of the 'configs' folder which includes various platform-dependent .cmake files
-export proxy_config_root=`pwd` #'/path/to/proxy-geos-hc'
-# Name of the config file used to pre-load the cache. The absolute path is ${proxy_config_root}/configs/${config_proxy}
+#Edit the script according to your setting. The default setting provided with this file assumes that the script is sourced from the proxy-geos-hc folder. 
+
+## proxy_config_root - the root of the 'configs' folder which includes various platform-dependent .cmake files. The default setting would search for a config files (machine specific and config_core.cmake) within proxy-geos-hc 
+export proxy_config_root=`pwd` #/path/to/proxy-geos-hc 
+
+## Name of the config file used to pre-load the cache. The absolute path is ${proxy_config_root}/configs/${config_proxy}. In case of failure a trial is given to ${proxy_config_root}/${config_proxy} also
 export config_proxy='config_x86_rtx2000.cmake'
 
-# For BUILD_FROM_TPLMIRROR=ON, the install folder for the TPLs. It is supposed to be a subfolder of $ENV{proxy_config_root}. 
-export install_tpl_dir=`pwd`/../proxy-geos-hc_tpl/installTPLs
+# The path to the folder where the TPLs are installed. Not required when installing the TPL from a package manager, i.e BUILD_FROM_TPLMIRROR=OFF
+# export install_tpl_dir=`pwd`/../proxy-geos-hc_tpl/installTPLs #/path/to/proxy-geos-hc_tpl/installTPLs
